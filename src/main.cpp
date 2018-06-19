ff904e443af760f944000d5b54d5e750cf28472f
Cameron Tacklind <cameron@tacklind.com>
2018-06-19 13:42:00 -0700

    Add pinspot pin definition

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@ constexpr int VariablePin       = A3;
 
 constexpr int RS485RxEnable     = 4; // PD4
 
+constexpr int PinSpot = 6; // PD6
+
 // ClearPath ENABLE;
 constexpr int EnablePin         = 10; // PB2
 // ClearPath Input A (DIRECTION);
